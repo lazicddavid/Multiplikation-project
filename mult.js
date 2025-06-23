86f48f715f872dca0ef277aa98bd768987618e9b
David Lazic <lazic.ddavid@gmail.com>
2025-06-23 11:43:39 +0200

    added title

---
+++ mult.js
@@ -0,0 +1,6 @@
+//izgenerisati 2 random broja, izmedju 1 i 10
+//dugmne za submit da je disejblano ukoliko korisnik nije ukucao nista
+//na submit proveriti je li to sto je ukucao korisnik, jednako proizvodu 2 random broja,
+//ukoliko jeste, povecati skor
+//ukoliko nije, smanjiti skor
+//u svakom slucaju, ponovo izgenerisati dva random broja
